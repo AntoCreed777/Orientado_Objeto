d767eb27b59dbb38308c7f58969736c095eda8ed
AntoCreed777 <abenavides@desem.cl>
2023-12-02 00:25:11 -0300

    aprendiendo entradas y salidas

--- Suelto/src/prueba.cpp
+++ Suelto/src/prueba.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+inline float volumen_cubo(float lado) {return lado*lado*lado;}
+
 int main(){
-    std::cout <<"Hola Mundo\nChao mundo"<<std::endl;
+    float lado;
+    std::cout <<"Introdusca el lado del Cubo:"<<std::endl;  //printf("");
+    std::cin>>lado; //scanf("%f",&lado);
+    std::cout<<"El volumen del cubo de lado "<<lado<<" es de "<<volumen_cubo(lado)<<std::endl;  //printf("%f",lado);
     return 0;
 }
